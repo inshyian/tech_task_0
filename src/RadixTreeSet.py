cd4d920ea99393cc7730d8208de913361ec64ca0
Ihor Shyian <inshyian@gmail.com>
2020-07-16 15:52:07 +0300

    implemented RadixTreeSet class; added unittests

---
+++ src/RadixTreeSet.py
@@ -0,0 +1,257 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 14 00:31:04 2020
+
+@author: Igorion
+"""
+
+
+# DictRadixTree
+
+_end = '_end_'
+
+class CodeNotExist(Exception):
+    pass
+
+class RadixTree():    
+    def __init__(self, *codes) -> None:
+        self.root = dict()
+        if len(codes) > 0:
+            for code in codes:
+                self.add(code)
+    
+    def __repr__(self) -> str:
+        codes = set()
+        self.__get_codes_depth(self.root, '', codes)
+        return "%s" % (codes)
+        # return "%s(%s)" % (self.__class__.__name__, codes)
+    
+    def __iter__(self) -> iter:
+        items = set()
+        self.__get_codes_depth(self.root, '', items)
+        return iter(items)
+    
+    def __len__(self) -> len:
+        items = set()
+        self.__get_codes_depth(self.root, '', items)
+        return len(items)
+    
+    def extend(self, codes) -> None:
+        for code in codes:
+            self.add(code)
+
+    def copy(self):
+        copy = RadixTree()
+        copy.root = self.root
+        return copy
+    
+    def clear(self):
+        self.root = dict()
+    
+    def union(self, args):
+        items = set()
+        self.__get_codes_depth(self.root, '', items)
+        new = self.copy()
+        for arg in args:
+            new.add(arg)
+        return new
+    
+    def difference(self, arg):
+        items = set()
+        self.__get_codes_depth(self.root, '', items)
+        new = RadixTree()
+        new.extend(items.difference(arg))
+        return new
+    
+    def difference_update(self, arg) -> None:
+        items = set()
+        self.__get_codes_depth(self.root, '', items)
+        items.difference_update(arg)
+        self.clear()
+        self.extend(items)
+    
+    def discard(self, arg) -> None:
+        items = set()
+        self.__get_codes_depth(self.root, '', items)
+        items.discard(arg)
+        self.clear()
+        self.extend(items)
+    
+    def intersection(self, arg):
+        items = set()
+        self.__get_codes_depth(self.root, '', items)
+        new = RadixTree()
+        new.extend(items.intersection(arg))
+        return new
+    
+    def intersection_update(self, arg) -> None:
+        items = set()
+        self.__get_codes_depth(self.root, '', items)
+        items.intersection_update(arg)
+        self.clear()
+        self.extend(items)
+    
+    def isdisjoint(self, arg) -> bool:
+        items = set()
+        self.__get_codes_depth(self.root, '', items)
+        return items.isdisjoint(arg)
+    
+    def issubset(self, arg) -> bool:
+        items = set()
+        self.__get_codes_depth(self.root, '', items)
+        return items.issubset(arg)
+    
+    def issuperset(self, arg) -> bool:
+        items = set()
+        self.__get_codes_depth(self.root, '', items)
+        return items.issuperset(arg)
+    
+    def pop(self) -> set():
+        items = set()
+        self.__get_codes_depth(self.root, '', items)
+        ret_item = items.pop()
+        self.clear()
+        self.extend(items)
+        return ret_item
+    
+    def remove(self, arg) -> None:
+        items = set()
+        self.__get_codes_depth(self.root, '', items)
+        items.remove(arg)
+        self.clear()
+        self.extend(items)
+        
+    def symmetric_difference(self, arg):
+        items = set()
+        self.__get_codes_depth(self.root, '', items)
+        new = RadixTree()
+        new.extend(items.symmetric_difference(arg))
+        return new
+    
+    def symmetric_difference_update(self, arg)-> None:
+        items = set()
+        self.__get_codes_depth(self.root, '', items)
+        items.symmetric_difference_update(arg)
+        self.clear()
+        self.extend(items)
+        
+    def update(self, *args) -> None:
+        for arg in args:
+            self.extend(arg)
+     
+    def add(self, code) -> None:           
+        current_dict = self.root
+        i = 0
+        while i < len(code):
+            keys = current_dict.keys()
+            # try to find matches key in current dictionary 
+            # (by comparing first char in current 'key' with current char in 'code')
+            for key in keys:
+                if key[0] == code[i]:      
+                    current_key = key
+                    ii = 0
+                    # iterators moves synchronously to skip same chars in 'code' and in 'current_key':
+                    while i < len(code) and ii < len(current_key) and code[i] == current_key[ii]:
+                        i += 1
+                        ii += 1
+                    # if 'code' and 'current_key' ended, creates _end entry (mark word's ending):
+                    if i == len(code) and ii == len(current_key):
+                        current_dict = current_dict[current_key]
+                        current_dict[_end] = _end
+                    # if 'code' ended, split's current_key, creates new dict, 
+                    # adds _end entry (to mark word's ending):
+                    elif i == len(code):
+                        current_dict[current_key[:ii]] = {current_key[ii:]:current_dict.pop(current_key)}
+                        current_dict[current_key[:ii]][_end] = _end
+                    # if 'current_key' ended, go depth to next dict:
+                    elif ii == len(current_key):
+                        current_dict = current_dict[current_key]
+                    # if code[i] != current_key[ii] split's 'current_key' and moves depth 
+                    # to new dict
+                    else:                  
+                        current_dict[current_key[:ii]] = {current_key[ii:]:current_dict.pop(current_key)}
+                        current_dict = current_dict[current_key[:ii]]
+                    break
+            # if failed to find matches key, create new dict {code[i:]:{_end:_end}}
+            else:       
+                current_dict = current_dict.setdefault(code[i:], {_end:_end})
+                i = len(code)
+         
+    def __get_codes_depth(self, root, prefix, codes_found) -> None:
+        if _end in root:
+            codes_found.add(prefix)
+        for key in root.keys():
+            if key != _end:
+                self.__get_codes_depth(root[key], prefix + key, codes_found)        
+    
+    def items(self) -> None:
+        codes_found = set()
+        self.__get_codes_depth(self.root, '', codes_found)
+        print(sorted(codes_found))
+        
+    def children(self, code) -> set():
+        codes_found = set()
+        prefix = ''
+        current_dict = self.root
+        i = 0
+        while i < len(code):
+            for key in current_dict.keys():
+                if key[0] == code[i]:
+                    ii = 0
+                    # iterators moves synchronously to skip same chars and writes
+                    # current char in 'code' to 'prefix' to save in 'codes_found'
+                    while i < len(code) and ii < len(key) and code[i] == key[ii]:
+                        prefix += code[i]
+                        i += 1
+                        ii += 1
+                    # call f to find and save all codes from current_dict[key] and deeper
+                    if i == len(code) and ii == len(key):
+                        self.__get_codes_depth(current_dict[key], code, codes_found)
+                    # call function to find and save all codes from current_dict[key] and deeper
+                    elif i == len(code):
+                        self.__get_codes_depth(current_dict[key], prefix + key[1:], codes_found)
+                    # go to deeper to next dict
+                    elif ii == len(key):
+                        current_dict = current_dict[key]
+                    elif code[i] != key[ii]:
+                        raise CodeNotExist()
+                    break
+            else:
+                raise CodeNotExist()
+        return codes_found              
+    
+    def parents(self, code) -> set():
+        codes_found = set()
+        prefix = ''
+        current_dict = self.root
+        i = 0
+        while i < len(code):
+            for key in current_dict.keys():
+                if key[0] == code[i]:
+                    ii = 0
+                    # iterators moves synchronously to skip same chars and writes
+                    # current char in 'code' to 'prefix' to save in 'codes_found'
+                    while i < len(code) and ii < len(key) and code[i] == key[ii]:
+                        prefix += code[i]
+                        i += 1
+                        ii += 1
+                    # if current 'key' is end of word add this to 'codes_found'
+                    if i == len(code) and ii == len(key):
+                        if _end in current_dict[key].keys():
+                            codes_found.add(prefix + key[1:])
+                        break
+                    # 'code' ended, word end did not reached -> nothing to add to 'codes_found'
+                    elif i == len(code):
+                        break
+                    # if current 'key' is end of word add this to 'codes_found'
+                    # and go deeper to next dict
+                    elif ii == len(key):
+                        if _end in current_dict[key].keys():
+                            codes_found.add(prefix + key[1:])
+                        current_dict = current_dict[key]
+                    elif code[i] != key[ii]:
+                        raise CodeNotExist()
+                    break
+            else:
+                raise CodeNotExist()
+        return codes_found
---
+++ unit_tests/test_RadixTreeSet.py
@@ -0,0 +1,202 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 16 08:54:26 2020
+
+@author: Igorion
+"""
+
+
+from src import RadixTreeSet
+
+def test_init():
+    a = RadixTreeSet.RadixTree('12', '13', '123', '12345', '123465', '145678')
+    b = {'12', '13', '123', '12345', '123465', '145678'}
+    assert repr(sorted(a)) == repr(sorted(b))
+
+def test_repr():
+    a = RadixTreeSet.RadixTree('a', 'asd3asd', 'dsf', '12312', '123', '1')
+    b = {'a', 'asd3asd', 'dsf', '12312', '123', '1'}
+    assert repr(sorted(a)) == repr(sorted(b))
+    
+def test_len():
+    a = RadixTreeSet.RadixTree('a', 'asd3asd', 'dsf', '12312', '123', '1')
+    b = {'a', 'asd3asd', 'dsf', '12312', '123', '1'}
+    assert len(a) == len(b)
+    
+def test_extend():
+    a = RadixTreeSet.RadixTree()
+    b = {'a', 'asd3asd', 'dsf', '12312', '123', '1', '123'}
+    a.extend(b)
+    assert repr(sorted(a)) == repr(sorted(b))
+    
+def test_copy():
+    a = RadixTreeSet.RadixTree('a', 'asd3asd', 'dsf', '12312', '123', '1')
+    b = a.copy()
+    assert a.root == b.root
+    
+def test_clear():
+    a = RadixTreeSet.RadixTree('a', 'asd3asd', 'dsf', '12312', '123', '1', '123')
+    b = RadixTreeSet.RadixTree()
+    a.clear()
+    assert repr(a) == repr(b)    
+    
+def test_union_iter():
+    a = RadixTreeSet.RadixTree('12', '13')
+    b = RadixTreeSet.RadixTree('12', '123')
+    c = {'12', '13'}
+    d = {'12', '123'}
+    assert sorted(a.union(b)) == sorted(c.union(d))
+    
+def test_difference():
+    a = RadixTreeSet.RadixTree('12', '13', '_')
+    b = RadixTreeSet.RadixTree('12', '123', '1234', 'a')
+    c = {'12', '13', '_'}
+    d = {'12', '123', '1234', 'a'}
+    assert sorted(a.difference(b)) == sorted(c.difference(d))
+    assert sorted(b.difference(a)) == sorted(d.difference(c))
+    assert sorted(a.difference(d)) == sorted(c.difference(b))
+    assert sorted(d.difference(a)) == sorted(b.difference(c))
+    
+def test_difference_update():
+    a = RadixTreeSet.RadixTree('12', '13', '_')
+    b = RadixTreeSet.RadixTree('12', '123', '1234', 'a')
+    c = {'12', '13', '_'}
+    d = {'12', '123', '1234', 'a'}
+    a.difference_update(b)
+    c.difference_update(d)
+    assert sorted(a) == sorted(c)
+    
+def test_discard():
+    a = RadixTreeSet.RadixTree('12', '13', '_')
+    b = RadixTreeSet.RadixTree('12', '123', '1234', 'a')
+    c = {'12', '13', '_'}
+    d = {'12', '123', '1234', 'a'}
+    a.discard(b)
+    c.discard(d)
+    assert sorted(a) == sorted(c)
+    
+def test_intersection():
+    a = RadixTreeSet.RadixTree('12', '13', '_')
+    b = RadixTreeSet.RadixTree('12', '123', '1234', 'a')
+    c = {'12', '13', '_'}
+    d = {'12', '123', '1234', 'a'} 
+    assert sorted(a.intersection(b)) == sorted(c.intersection(d))    
+    
+def test_intersection_update():
+    a = RadixTreeSet.RadixTree('12', '13', '_')
+    b = RadixTreeSet.RadixTree('12', '123', '1234', 'a')
+    c = {'12', '13', '_'}
+    d = {'12', '123', '1234', 'a'}
+    a.intersection_update(b)
+    c.intersection_update(d)
+    assert sorted(a) == sorted(c)
+    
+def test_isdisjoint():
+    a = RadixTreeSet.RadixTree('12', '13', '_')
+    b = RadixTreeSet.RadixTree('12', '14', '15', 'a')
+    c = RadixTreeSet.RadixTree('112', '126', '15', 'c')
+    d = {'12', '13', '_'}
+    e = {'12', '14', '15', 'a'}
+    f = {'112', '126', '15', 'c'}
+    assert a.isdisjoint(b) == d.isdisjoint(e)
+    assert a.isdisjoint(c) == d.isdisjoint(f)
+    assert b.isdisjoint(c) == e.isdisjoint(f)
+    
+def test_issubset():
+    a = RadixTreeSet.RadixTree('12', '13', '14')
+    b = RadixTreeSet.RadixTree('12', '13', '15', '14')
+    c = RadixTreeSet.RadixTree('112', '12', '13', '14')
+    d = {'12', '13', '14'}
+    e = {'12', '13', '15', '14'}
+    f = {'112', '12', '13', '14'}
+    assert a.issubset(b) == d.issubset(e)
+    assert a.issubset(c) == d.issubset(f)
+    assert b.issubset(c) == e.issubset(f)
+    
+def test_issuperset():
+    a = RadixTreeSet.RadixTree('12', '13', '14', '15')
+    b = RadixTreeSet.RadixTree('12', '13', '14')
+    c = RadixTreeSet.RadixTree('112', '12', '13', '14')
+    d = {'12', '13', '14', '15'}
+    e = {'12', '13', '14'}
+    f = {'112', '12', '13', '14'}
+    assert a.issuperset(b) == d.issuperset(e)
+    assert a.issuperset(c) == d.issuperset(f)
+    assert b.issuperset(c) == e.issuperset(f)
+    
+    # fails sometimes with more than one item, probably due to different order in set() and RadixTree()
+def test_pop():
+    a = RadixTreeSet.RadixTree('15')
+    b = {'15'}
+    assert a.pop() == b.pop()
+    assert sorted(a) == sorted(b)
+    
+def test_remove():
+    a = RadixTreeSet.RadixTree('12', '13', '14', '15')
+    b = {'12', '13', '14', '15'}
+    a.remove('13')
+    b.remove('13')
+    assert sorted(a) == sorted(b)
+    
+def test_symmetric_difference():
+    a = RadixTreeSet.RadixTree('12', '13', '14', '15')
+    b = RadixTreeSet.RadixTree('12', '13', '18', '20')
+    c = {'12', '13', '14', '15'}
+    d = {'12', '13', '18', '20'}
+    assert sorted(a.symmetric_difference(b)) == sorted(c.symmetric_difference(d))
+    
+def test_symmetric_difference_update():
+    a = RadixTreeSet.RadixTree('12', '13', '14', '15')
+    b = RadixTreeSet.RadixTree('12', '13', '18', '20')
+    c = {'12', '13', '14', '15'}
+    d = {'12', '13', '18', '20'}
+    a.symmetric_difference(b)
+    c.symmetric_difference(d)
+    assert sorted(a) == sorted(c)
+    
+def test_update():
+    a = RadixTreeSet.RadixTree('1')
+    b = RadixTreeSet.RadixTree('2')
+    c = RadixTreeSet.RadixTree('13', '14')
+    a_ = {'1'}
+    b_ = {'2'}
+    c_ = {'13', '14'}
+    c__ = ['13', '14', '1000']
+    a.update(b)
+    a_.update(b)
+    assert sorted(a) == sorted(a_)
+    a.update(b_, c_, c__)
+    a_.update(b, c, c__)
+    assert sorted(a) == sorted(a_)
+    
+def test_add():
+    a = RadixTreeSet.RadixTree('a', 'asd3asd', 'dsf', '12312', '123', '1', '123')
+    b = RadixTreeSet.RadixTree()
+    b.add('asd3asd')
+    b.add('a')
+    b.add('123')
+    b.add('1')
+    b.add('12312')
+    b.add('dsf')
+    c = {'a', 'asd3asd', 'dsf', '12312', '123', '1', '123'}
+    assert a.root == b.root
+    assert repr(sorted(b)) == repr(sorted(c))
+      
+def test_items(capsys):
+    a = RadixTreeSet.RadixTree('a', 'asd3asd', 'dsf', '12312', '123', '1', '123')
+    b = {'a', 'asd3asd', 'dsf', '12312', '123', '1', '123'}
+    a.items()
+    captured = capsys.readouterr()
+    a_out = captured.out
+    print(sorted(b))
+    captured = capsys.readouterr()
+    b_out = captured.out
+    assert a_out == b_out
+    
+def test_children():
+    a = RadixTreeSet.RadixTree('12', '13', '123', '12345', 'a', '123465', '145678')
+    assert a.children('123') == {'123', '12345', '123465'}
+
+def test_parents():
+    a = RadixTreeSet.RadixTree('12', '13', '123', '12345', 'a', '123465', '145678')
+    assert a.parents('1234') == {'12', '123'}
